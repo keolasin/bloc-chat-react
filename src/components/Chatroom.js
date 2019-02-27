285ef2661234153fb999d4da7d8f2167f24a12c9
Matthew Reyes <43076895+keolasin@users.noreply.github.com>
2019-02-27 11:19:26 -0800

    timeConvert and message JSX
    JSX placeholders, need to still sort the messages according to the: roomId (matching activeRoom), and the time the message is sent

--- src/components/Chatroom.js
+++ src/components/Chatroom.js
@@ -6,28 +6,53 @@ class Chatroom extends Component {
     super(props);
     this.state = {
       value: '',
-      messages: []
+      messages: [],
+      sortedMessages: []
     }
-
-    this.roomsRef = this.props.firebase.database().ref('rooms');
+    
     this.messagesRef = this.props.firebase.database().ref('Messages');
   }
 
   componentDidMount(){
     this.messagesRef.on('child_added', snapshot => {
       const message = snapshot.val();
-      this.setState( { messages: this.state.messages.concat(message.key) } );
+      this.setState( { messages: this.state.messages.concat(message) } );
     });
+  }
+  
+  timeConverter(message){
+    let date = new Date(message.child('sentAt'));
+    let hour = date.getHour();
+    let minutes = '0'+date.getMinutes();
+    let seconds = '0'+date.getSeconds();
+    
+    return formattedTime = `${hour}:${minutes.substr(-2)}:${seconds.substr(-2)} on ${date}`;
   }
 
   render(){
     return(
-      <section className='chat-container'>
-        <h2>{this.props.activeRoom}</h2>
-        <content>
-
-        </content>
-      </section>
+      <main className='chat-container'>
+        <header>
+          <h1 classname='active-room'>{this.props.activeRoom.name}</h2>
+        </header>
+        <article>
+          {
+            this.state.messages.map( (message, this.props.activeRoom) => {
+              if (message.child('roomId') === this.props.activeRoom.key){
+                return (
+                  <section className='message'>
+                   <h1 className='message-userName'>{ message.child('userName') }</h1>
+                   <p className='message-content'>{ message.child('content') }</p>
+                   <h2 className='message-sentAt'>{ message.child('sentAt') }</h2>
+                 </section>
+                );
+              } else {
+                return null;
+              }
+            });
+          }
+        </article>
+      </main>
     );
   }
 }
