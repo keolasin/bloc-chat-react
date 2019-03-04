864041b56f2760d38e2edea43815a3b00f3ee198
Matthew Reyes <43076895+keolasin@users.noreply.github.com>
2019-03-04 14:53:15 -0800

    submit msg wip

--- src/components/MessageField.js
+++ src/components/MessageField.js
@@ -4,6 +4,37 @@ import ReactDOM from 'react-dom';
 class MessageList extends Component {
   constructor(props){
     super(props);
-
+    this.state = {
+      value=''
+    }
+  }
+  
+  // typing in the enter message form field
+  handleChange(event){
+    this.setState( {value: event.target.value} );
+  }
+  
+  // adding the submitted message to the firebase database
+  handleMessageSubmit(event){
+    this.props.messages.push({
+      
+    })
+  }
+  
+  render(){
+    return(
+      <section className='message-form-container'>
+      <form onSubmit={(event)=>this.handleMessageSubmit(event)}>
+          <label htmlFor='text' id='input-message-field'>
+            <input type='text'
+                   placeholder='Enter your message here.'
+                   id ='input-message-field'
+                   value={this.state.value}
+                   onChange={(event)=>this.handleChange(event)}/>
+          </label>
+          <input id='submit-message' type="submit" value="Send message" />
+        </form>
+      </section>
+    );
   }
 }
