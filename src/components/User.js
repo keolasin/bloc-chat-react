99086b7c8af16182311e059f5fdb948c75dad1ca
Keolasin <43076895+keolasin@users.noreply.github.com>
2019-03-04 20:22:40 -0800

    merge createMsg

--- src/components/User.js
+++ src/components/User.js
@@ -4,21 +4,6 @@ import ReactDOM from 'react-dom';
 class User extends Component {
   constructor(props){
     super(props);
-<<<<<<< HEAD
-    this.state = {
-      user: ''
-    }
-
-    const provider = new this.props.firebase.auth.GoogleAuthProvider();
-  }
-
-  componentDidMount(){
-
-  }
-
-  handleSignInClick(){
-    this.setState( {} );
-=======
   }
 
   componentDidMount(){
@@ -38,15 +23,10 @@ class User extends Component {
 
   handleSignOutClick(){
     this.props.firebase.auth().signOut();
->>>>>>> c0affbec8cf7b0b9b6f538e885375c3b150224cc
   }
 
   render(){
     return(
-<<<<<<< HEAD
-      <section className='user-sign-in'>
-        <button>Sign in</button>
-=======
       <section className='user-signIn'>
         <h1>{ this.props.user ? this.props.user.displayName : 'Guest' /*display the username */}</h1>
         {// change sign-in/sign-out button depending if the user is signed in/out
@@ -54,7 +34,6 @@ class User extends Component {
           : <button onClick={()=>this.handleSignInClick()}>Sign in</button>
         }
 
->>>>>>> c0affbec8cf7b0b9b6f538e885375c3b150224cc
       </section>
     );
   }
